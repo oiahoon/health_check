e6cff84c1b8ee95a84cfa899c963f731da4a4fe3
Joey (Yuyao) Huang <johuang@aligntech.com>
2021-01-15 00:30:01 +0800

    fix /status error

--- lib/health_check/health_check_routes.rb
+++ lib/health_check/health_check_routes.rb
@@ -8,8 +8,8 @@ module ActionDispatch::Routing
 
     def add_health_check_routes(prefix = nil)
       HealthCheck.uri = prefix if prefix
-      match "#{HealthCheck.uri}(/:checks)(.:format)", :to => 'health_check/health_check#index', via: [:get, :post], :defaults => { :format => 'json' }
       match "#{HealthCheck.uri}(/status)(.:format)", :to => 'health_check/health_check#status', via: [:get, :post], :defaults => { :format => 'json' }
+      match "#{HealthCheck.uri}(/:checks)(.:format)", :to => 'health_check/health_check#index', via: [:get, :post], :defaults => { :format => 'json' }
     end
 
   end
